9d0094e8c23e1c262ca8253ae2da49db7f5bd61f
k bouabene <k.bouabene@gmail.com>
2019-04-06 13:31:57 +0200

    Update main.tf
    live demo

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -55,7 +55,7 @@ resource "aws_launch_configuration" "webserver_example" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, TSL" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
